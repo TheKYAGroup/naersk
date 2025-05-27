37b1bc5377d0fb1b15d0f083712f87dc1f04fcc6
Ethan Nel <ethan.nel@thekyagroup.com>
2025-05-27 09:19:55 -0700

    Updated flake.nix to allow building in debug or release

--- examples/hello-world/flake.nix
+++ examples/hello-world/flake.nix
@@ -3,9 +3,13 @@
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
+  outputs = { self, flake-utils, naersk, nixpkgs, nixpkgs-mozilla }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
@@ -14,15 +18,32 @@
 
         naersk' = pkgs.callPackage naersk {};
 
+	buildPackageFor = releaseMode: naersk'.buildPackage {
+	  src = ./.;
+	  release = releaseMode;
+	  # openssl_3 is reuired for SageSdk
+          nativeBuildInputs = with pkgs; [ makeWrapper openssl_3 ];
+	  postInstall = ''
+	    wrapProgram $out/bin/hello-world\
+              --set LD_LIBRARY_PATH ${pkgs.lib.makeLibraryPath [ pkgs.openssl_3 ]}
+	  ''
+	}
+
       in rec {
         # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-        };
+        defaultPackage = debugPackage;
+
+	# Debug build
+        packages.debug = debugPackage;
+	# Release mode
+        packages.release = releasePackage;
+
+        debugPackage = buildPackageFor false;
+        releasePackage = buildPackageFor true;
 
         # For `nix develop`:
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
+          nativeBuildInputs = with pkgs; [ rustc cargo openssl_3 ];
         };
       }
     );
